adc4a145978c439b95f814f410b4950d623704aa
YaGrig <yashagrigorev@gmail.com>
2021-03-18 21:06:31 +0300

    feat: add eventListener on click

--- virtual-piano/script.js
+++ virtual-piano/script.js
@@ -9,3 +9,23 @@ function playAudio(src) {
     audio.currentTime = 0;
     audio.play();
   }
+function endtransition(e){
+  if(e.propertyName !=='background-color') return;
+  if (this.classList.contains('sharplaying')){
+  this.classList.remove('sharplaying')
+} else {
+  this.classList.remove('playing');
+}
+}
+pianokeys.forEach(key => key.addEventListener('transitionend', endtransition))
+
+pianokeys.forEach(key => key.addEventListener('click', (event) => {
+      const note = event.target.dataset.note;
+      const src = `assets/audio/${note}.mp3`;
+      const key = document.querySelector(`.piano-key[data-note="${note}"]`);
+      playAudio(src);
+      if(!key.classList.contains('sharp')){
+        key.classList.add('playing')
+      }
+      else {key.classList.add('.piano-key.sharplaying')}
+  }))
